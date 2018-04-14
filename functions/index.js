42e5696e82326d711275bf353dbbe2c72237e477
Manuel <menrique.gutierrez@gmail.com>
2018-04-13 19:38:58 -0500

    Tests Not Working -- Added Axios to handle http requests.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,59 @@
-const functions = require('firebase-functions');
+// const functions = require('firebase-functions');
+// const admin = require('firebase-admin');
+// const axios = require('axios');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+// /**
+//  * A simple text test assertion to check it is working
+//  */
+
+// exports.test = () => {
+//     return "hello";
+// };
+
+// /**
+//  * Responds to any HTTP request that can provide a "message" field in the body.
+//  *
+//  * @param {Object} req Cloud Function request context.
+//  * @param {Object} res Cloud Function response context.
+//  */
+
+// exports.helloWorld = (req, res) => {
+//     if (req.body.message === undefined) {
+//       // This is an error case, as "message" is required
+//       res.status(400).send('No message defined!');
+//     } else {
+//       // Everything is ok
+//       console.log(req.body.message);
+//       res.status(200).end();
+//     }
+//   };
+
+
+//  /**
+//  * Make a test request.
+//  *
+//  * @param {Object} req Cloud Function request context.
+//  * @param {Object} res Cloud Function response context.
+//  */
+
+
+// exports.test1 = () => {
+
+//     // const instance = axios.create({
+//     //     baseURL: 'https://us18.api.mailchimp.com/3.0/',
+//     //     timeout: 3000,
+//     //     headers: {'Authorization':'apikey 7de1ffec50d8b6ae4e45a193dd1bbf93-us18'}
+//     // });
+
+//     // instance.get('/lists/428fd40657/members')
+//     //   .then(response => {
+//     // //    console.log(response.data.explanation);
+//     //    return console.log(response.data.members[0].id);
+//     //   })
+//     //   .catch(error => {
+//     //     return console.log(error); 
+//     //   });
+// }
+
+
+
---
+++ functions/test/test.js
@@ -0,0 +1,186 @@
+let chai = require('chai');
+let expect = chai.expect;
+
+// describe('#Facebook TypingOff method ', () => {
+
+//     let token = k.facebook.token;
+// 	let userId = k.facebook.userId;
+	 
+// 	it("Should fail on bad access token error 400", function(done) {
+
+// 		let badToken = "aiosdhasidj"
+// 		const facebook = new facebookRequest(badToken, userId);
+
+// 	    let testPromise = new Promise(function(resolve, reject) {
+// 	    	resolve(facebook.typingOff());
+// 	    });
+
+// 		testPromise
+// 		.then((result) => {
+// 	        //Show the assertion ok
+// 	        expect(result.statusCode).to.equal(400);
+// 	        done()
+// 	    })
+// 	    .catch((error) => {
+// 	    	// show assertion error
+// 	    	done(error);
+// 	    })
+// 	});
+
+
+
+// });
+
+
+
+
+// const resolvingPromise = new Promise((resolve) =>
+//   resolve('promise resolved')
+// );
+// const rejectingPromise = new Promise((resolve, reject) =>
+//   reject(new Error('promise rejected'))
+// );
+
+// /*
+// When you assert on the result of a promise,
+// there are several failure modes you want to test for.
+// This is what can go wrong:
+//  - Your assertion on the result of the promise can fail.
+//  - The promise you test can reject unexpectedly.
+// */
+
+// describe('testing promises', () => {
+
+//   describe('with "done" callback', () => {
+
+//     //Output: ✓ assertion success
+//     it('assertion success', (done) => {
+//       resolvingPromise.then( (result) => {
+//         expect(result).to.equal('promise resolved');
+//         done();
+//       });
+//     });
+
+//     //Output: Error: timeout of 2000ms exceeded. Ensure done() is being called.
+//     it('failing assertion', (done) => { // Don't forget 'done' here...
+//       resolvingPromise.then( (result) => {
+//         expect(result).to.equal('i fail');
+//         done();
+//       });
+//     });
+
+//     //Output: Error: timeout of 2000ms exceeded. Ensure done() is being called.
+//     it('promise rejects', (done) => { // Don't forget 'done' here...
+//       rejectingPromise.then( (result) => {
+//         expect(result).to.equal('promise resolved');
+//         done();
+//       });
+//     });
+
+//     /*
+//     There are two problems with this style:
+//      - You won't see the cause of your test test failures
+//      - there is nothing holding you back from writing an evergreen test. Try
+//        removing 'done' on line 38 and 46 and be amazed if you run 'npm test'
+//     */
+//   });
+
+//   describe('"done" callback with .then(done,done)', () => {
+
+//     //Output: ✓ assertion success
+//     it('assertion success', (done) => {
+//       resolvingPromise.then( (result) => {
+//         expect(result).to.equal('promise resolved');
+//       }).then(done,done);
+//     });
+
+//     //Output: AssertionError: expected 'promise resolved' to equal 'i fail'
+//     it('failing assertion', (done) => {
+//       resolvingPromise.then( (result) => {
+//         expect(result).to.equal('i fail');
+//       }).then(done,done);
+//     });
+
+//     //Output: Error: promise rejected
+//     it('promise rejects', (done) => {
+//       rejectingPromise.then( (result) => {
+//         expect(result).to.equal('promise resolved');
+//       }).then(done,done);
+//     });
+
+//     /*
+//     If you want to use the 'done' callback, this is the best way.
+//      - Your test failures are caught and displayed
+//      - When you forget to supply 'done' as argument, you will get
+//        'done is not defined'
+//      - If you forget to end your test with '.then(done,done)</code>',
+//        mocha warns you about a missing 'done'.
+//     */
+
+//   });
+
+//   describe('return a promise', () => {
+
+//     //Output: ✓ assertion success
+//     it('assertion success', () => {
+//       return resolvingPromise.then( (result) => {
+//         expect(result).to.equal('promise resolved');
+//       });
+//     });
+
+//     //Output: AssertionError: expected 'promise resolved' to equal 'i fail'
+//     it('failing assertion', () => {
+//       return resolvingPromise.then( (result) => {
+//         expect(result).to.equal('i fail');
+//       });
+//     });
+
+//     //Output: Error: promise rejected
+//     it('promise rejects', () => {
+//       return rejectingPromise.then( (result) => {
+//         expect(result).to.equal('promise resolved');
+//       });
+//     });
+
+//     //More readable with chai-as-promised
+//     it('succes with chai as promised', () => {
+//        return resolvingPromise.should.eventually.equal("promise resolved");
+//     });
+
+//     /*
+//      Meh. When you forget to type 'return', your test is evergreen and
+//      there is no safeguard to warn you about the missing return.
+//      */
+
+//   });
+
+//   describe('async await', () => {
+
+//     //Output: ✓ assertion success
+//     it('assertion success', async () => {
+//       const result = await resolvingPromise;
+//       expect(result).to.equal('promise resolved');
+//     });
+
+//     //Output: AssertionError: expected 'promise resolved' to equal 'i fail'
+//     it('failing assertion', async () => {
+//       const result = await resolvingPromise;
+//       expect(result).to.equal('i fail');
+//     });
+
+//     //Output: Error: promise rejected
+//     it('promise rejects', async () => {
+//       const result = await rejectingPromise;
+//       expect(result).to.equal('promise resolved');
+//     });
+
+//     /*
+//     This style is just as safe as the '.then(done,done)' style,
+//     but it's much easier to read since there is no chaining and nesting
+//     of then callbacks.
+//     */
+
+//   });
+
+
+// });
